eaf9969efe2afc80df13ce310d83e421953fe342
bluemoonexporters <bluemoonexporters351@gmail.com>
2024-06-24 23:10:15 +0530

    Dev Branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bluemoon Exporters
+          Under Maintenance
         </p>
       </header>
     </div>
