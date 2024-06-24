484ea8ce431c99180fbfcb4dbd85952ccd82f912
dev1 <dev1@bluemoonexporters.com>
2024-06-25 01:55:45 +0530

    Initial Carousel

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,4 +1,7 @@
-import { Container, Navbar} from 'react-bootstrap'
+import { Container, Navbar, Carousel, Nav } from 'react-bootstrap'
+import crops_img from "../utils/img/crops.png"
+import orange_img from "../utils/img/orange.png"
+import flower_img from "../utils/img/flower.png"
 
 const Page = () => {
     return (
@@ -6,13 +9,39 @@ const Page = () => {
         <Navbar bg="dark">
             <Container>
                 <Navbar.Brand href="#home">Bluemoon Exporters</Navbar.Brand>
-                {/*
-                <Nav className="me-auto justify-content-end">
-                    <Nav.Link href="#contact_us">Contact Us</Nav.Link>
-                </Nav>
-                */}
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto justify-content-end">
+                            <Nav.Link href="#contact_us">Contact Us</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
             </Container>
         </Navbar>
+        <Container>
+            <Carousel class='text-white'>
+                <Carousel.Item>
+                    <img className='d-block w-100' src={crops_img} alt='First slide'/>
+                    <Carousel.Caption>
+                        <h3>Premium Quality Coffee Beans</h3>
+                        <p>Discover the rich aroma and bold flavor of our premium quality coffee beans. Sourced from the finest coffee plantations, our beans are carefully selected and processed to ensure the highest standards of excellence. Perfect for coffee enthusiasts and connoisseurs, our coffee beans are exported globally, bringing a taste of perfection to every cup</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='d-block w-100' src={orange_img} alt='Second slide'/>
+                    <Carousel.Caption>
+                        <h3>Fresh Fruits</h3>
+                        <p>Experience the burst of freshness with our export-quality citrus fruits. Our oranges, lemons, and grapefruits are grown in sun-kissed orchards, ensuring they are packed with flavor and nutrients. Handpicked and meticulously packaged, our citrus fruits reach international markets in peak condition, providing a refreshing and healthy option for consumers worldwide.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='d-block w-100' src={flower_img} alt='Third slide'/>
+                    <Carousel.Caption>
+                        <h3>Organic Avocados</h3>
+                        <p>Our avocados are organically grown in lush, nutrient-rich soils, ensuring the best taste and health benefits.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
         </>
     )
 }
